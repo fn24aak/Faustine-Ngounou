d41c227b44f9826ba80b3840e377297d46774840
fn24aak <fn24aak@COM-LG2-18.herts.ac.uk>
2026-02-23 11:47:57 +0000

    we make a Complex class

---
+++ git_proj_1/src/main/java/com/mycompany/git_proj_1/Complex.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.git_proj_1;
+
+/**
+ *
+ * @author fn24aak
+ */
+public class Complex {
+    
+}
