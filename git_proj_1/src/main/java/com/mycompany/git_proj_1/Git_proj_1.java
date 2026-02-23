ba168c50b3aff01e2222530b6e4be8479f6aad08
sr24abc <sr24abc@COM-LG2-22.herts.ac.uk>
2026-02-23 11:35:36 +0000

    initial commit - with team

---
+++ git_proj_1/src/main/java/com/mycompany/git_proj_1/Git_proj_1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.git_proj_1;
+
+/**
+ *
+ * @author sr24abc
+ */
+public class Git_proj_1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
